a7356448a0acbeee1e6fa1a7f3b22234ae912ca3
Renchunzi-Xie <renchunzi.xie@gmail.com>
2023-12-28 11:05:39 +0800

    camera ready

--- data/utils.py
+++ data/utils.py
@@ -1,16 +1,8 @@
 from data.cifar10 import *
 from data.cifar100 import *
 from data.tinyimagenet import *
-from data.imagenet_subset import *
-from data.imagenet import *
-from data.pacs import *
-from data.office31 import *
-from data.offce_home import *
-from data.wilds_FMoW import *
-from data.wilds_camelyon17 import *
 import torch
-from data.wilds_rr1 import *
-from data.breeds import *
+
 
 def build_dataloader(dataname, args):
     random_seeds = torch.randint(0, 10000, (2,))
